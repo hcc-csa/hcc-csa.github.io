c2d211f2b8910e595fd2715b1cabbac6eb9d0cac
Gabriel Morales <gabbomorales1@gmail.com>
2024-03-01 11:59:45 -0600

    setup: added django boilerplate and turbo monorepo

--- apps/client/astro.config.mjs
+++ apps/client/astro.config.mjs
@@ -4,5 +4,13 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  server: {
+    proxy: {
+      '/api': {
+        'target': '',
+        changeOrigin: true
+      }
+    }
+  }
 });
