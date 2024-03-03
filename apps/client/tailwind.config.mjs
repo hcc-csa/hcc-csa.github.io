00bcee99dceb9eb32a18f9d747cd1f3f470894a8
Hasala2002 <hasala.h@hotmail.com>
2024-03-03 12:43:26 -0600

    feat: added Header component && Hero Layout

--- apps/client/tailwind.config.mjs
+++ apps/client/tailwind.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      width: {
+        400: '400px'
+      },
+      height: {
+        400: '400px'
+      },
+      backdropBlur: {
+        '5xl': '250px'
+      }
+    },
+    backgroundSize: {
+      '75%': '75%'
+    }
+  },
+  plugins: []
 }
