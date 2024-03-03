bf766ca026e259a34af7a33a30c26d93e82fc6f9
Hasala2002 <hasala.h@hotmail.com>
2024-03-03 10:33:27 -0600

    setup: added react and integrated tailwind

--- apps/client/astro.config.mjs
+++ apps/client/astro.config.mjs
@@ -1,16 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
+import react from '@astrojs/react'
+
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [react(), tailwind()],
   server: {
     proxy: {
       '/api': {
-        'target': '',
+        target: '',
         changeOrigin: true
       }
     }
   }
-});
+})
