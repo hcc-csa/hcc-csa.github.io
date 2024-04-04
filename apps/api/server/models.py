9652ee7fc1b0416ae0522b15aa984446c8250793
Gabriel Morales <gabbomorales1@gmail.com>
2024-04-04 16:11:34 -0500

    feat: added app settings and event model

--- apps/api/server/models.py
+++ apps/api/server/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    event_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+    def event_has_passed(self):
+        return self.event_date < timezone.now()
