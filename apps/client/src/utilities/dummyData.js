05ad9f2d03e9d6f0e44668086604b5aafd4c9aa7
Hasala2002 <hasala.h@hotmail.com>
2024-04-15 23:09:07 -0500

    feat: dynamically generated content for Events/Gallery/Officials

---
+++ apps/client/src/utilities/dummyData.js
@@ -0,0 +1,164 @@
+export const officials = [
+  {
+    name: 'Ariyan Behrouz',
+    position: 'President',
+    quote: 'lorem ipsum',
+    image: 'https://i.ibb.co/gSK6gBR/image-43.png',
+    socials: {
+      github: 'https://tinyurl.com/5x2bcwjy',
+      discord: 'https://tinyurl.com/5x2bcwjy',
+      mail: 'https://tinyurl.com/5x2bcwjy',
+      website: 'https://tinyurl.com/5x2bcwjy'
+    }
+  },
+  {
+    name: 'Ariyan Behrouz',
+    position: 'President',
+    quote: 'lorem ipsum',
+    image: 'https://i.ibb.co/gSK6gBR/image-43.png',
+    socials: {
+      // github: 'https://tinyurl.com/5x2bcwjy',
+      discord: 'https://tinyurl.com/5x2bcwjy',
+      mail: 'https://tinyurl.com/5x2bcwjy',
+      website: 'https://tinyurl.com/5x2bcwjy'
+    }
+  },
+  {
+    name: 'Ariyan Behrouz',
+    position: 'President',
+    quote: 'lorem ipsum',
+    image: 'https://i.ibb.co/gSK6gBR/image-43.png',
+    socials: {
+      github: 'https://tinyurl.com/5x2bcwjy',
+      discord: 'https://tinyurl.com/5x2bcwjy',
+      mail: 'https://tinyurl.com/5x2bcwjy',
+      website: 'https://tinyurl.com/5x2bcwjy'
+    }
+  },
+  {
+    name: 'Ariyan Behrouz',
+    position: 'President',
+    quote: 'lorem ipsum',
+    image: 'https://i.ibb.co/gSK6gBR/image-43.png',
+    socials: {
+      github: 'https://tinyurl.com/5x2bcwjy',
+      discord: 'https://tinyurl.com/5x2bcwjy',
+      mail: 'https://tinyurl.com/5x2bcwjy',
+      website: 'https://tinyurl.com/5x2bcwjy'
+    }
+  }
+]
+
+export const events = [
+  {
+    id: 'E12312312',
+    title: 'Deep Fake Technology',
+    description:
+      'Are you ready to dive into the world of Artificial Intelligence? Join us for an exclusive event focused on the captivating realm of Deep Fake Technology! Dive into the fascinating intersection of AI and creativity, we present the most awaited collaboration of the year HCC Cyber Club x HCC Computer Science Association (CSA) with the biggest clubs at Houston Community College!',
+    event_date: new Date('May 1, 2024 18:00:00'),
+    address: `Houston Community College
+Room C 145,
+West Loop Campus,
+West Loop South,
+Houston, TX`,
+    short_address: '5601 W Loop S, Houston',
+    time_end: '8:00pm',
+    registration_link: 'https://tinyurl.com/5x2bcwjy',
+    image: 'https://i.ibb.co/h9Bs2p8/NAFEB232024-1.png'
+  },
+  {
+    id: 'E12312223',
+    title: 'Deep Fake Technology',
+    description:
+      'Are you ready to dive into the world of Artificial Intelligence? Join us for an exclusive event focused on the captivating realm of Deep Fake Technology! Dive into the fascinating intersection of AI and creativity, we present the most awaited collaboration of the year HCC Cyber Club x HCC Computer Science Association (CSA) with the biggest clubs at Houston Community College!',
+    event_date: new Date('February 23, 2024 18:00:00'),
+    address: `Houston Community College
+Room C 145,
+West Loop Campus,
+West Loop South,
+Houston, TX`,
+    short_address: '5601 W Loop S, Houston',
+    time_end: '8:00pm',
+    registration_link: 'https://tinyurl.com/5x2bcwjy',
+    image: 'https://i.ibb.co/h9Bs2p8/NAFEB232024-1.png'
+  },
+  {
+    id: 'E123142312',
+    title: 'Deep Fake Technology',
+    description:
+      'Are you ready to dive into the world of Artificial Intelligence? Join us for an exclusive event focused on the captivating realm of Deep Fake Technology! Dive into the fascinating intersection of AI and creativity, we present the most awaited collaboration of the year HCC Cyber Club x HCC Computer Science Association (CSA) with the biggest clubs at Houston Community College!',
+    event_date: new Date('February 23, 2024 18:00:00'),
+    address: `Houston Community College
+Room C 145,
+West Loop Campus,
+West Loop South,
+Houston, TX`,
+    short_address: '5601 W Loop S, Houston',
+    time_end: '8:00pm',
+    registration_link: 'https://tinyurl.com/5x2bcwjy',
+    image: 'https://i.ibb.co/h9Bs2p8/NAFEB232024-1.png'
+  }
+]
+
+export const albums = [
+  {
+    id: 'A12345',
+    title: 'CSA Hackathon',
+    description:
+      'Lorem ipsum dolor sit amet. Est deserunt consequatur id soluta aliquam ut fuga recusandae in dolores eaque qui eligendi architecto et incidunt officia.',
+    date: new Date('October 20, 2023'),
+    photos: [
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png'
+    ]
+  },
+  {
+    id: 'A23456',
+    title: 'CSA Hackathon',
+    description:
+      'Lorem ipsum dolor sit amet. Est deserunt consequatur id soluta aliquam ut fuga recusandae in dolores eaque qui eligendi architecto et incidunt officia.',
+    date: new Date('October 20, 2023'),
+    photos: [
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png'
+    ]
+  },
+  {
+    id: 'A34567',
+    title: 'CSA Hackathon',
+    description:
+      'Lorem ipsum dolor sit amet. Est deserunt consequatur id soluta aliquam ut fuga recusandae in dolores eaque qui eligendi architecto et incidunt officia.',
+    date: new Date('October 20, 2023'),
+    photos: [
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png'
+    ]
+  },
+  {
+    id: 'A45678',
+    title: 'CSA Hackathon',
+    description:
+      'Lorem ipsum dolor sit amet. Est deserunt consequatur id soluta aliquam ut fuga recusandae in dolores eaque qui eligendi architecto et incidunt officia.',
+    date: new Date('October 20, 2023'),
+    photos: [
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://www.giantfreakinrobot.com/wp-content/uploads/2022/08/rick-astley.jpg'
+    ]
+  }
+]
