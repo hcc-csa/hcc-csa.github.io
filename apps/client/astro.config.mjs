59045fbe853636c8818e7838f24e459c78b48fd8
Hasala2002 <hasala.h@hotmail.com>
2024-06-14 21:51:34 -0500

    feat: added Sanity to Astro App

--- apps/client/astro.config.mjs
+++ apps/client/astro.config.mjs
@@ -1,12 +1,21 @@
 import { defineConfig } from 'astro/config'
-
 import react from '@astrojs/react'
-
 import tailwind from '@astrojs/tailwind'
+
+import sanity from '@sanity/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [
+    react(),
+    tailwind(),
+    sanity({
+      projectId: '128fvw6m',
+      dataset: 'production',
+      useCdn: true,
+      apiVersion: 'v2022-03-07'
+    })
+  ],
   server: {
     proxy: {
       '/api': {
