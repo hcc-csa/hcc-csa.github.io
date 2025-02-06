aec3c9301c2e28fecee38d74c66bd70b3f121b3a
Lex <58815784+avonbied@users.noreply.github.com>
2025-02-05 20:25:53 -0600

    Add Current Site

---
+++ src/utilities/dummyData.js
@@ -0,0 +1,227 @@
+export const officials = [
+  {
+    name: 'Ariyan Behrouz',
+    position: 'President',
+    quote: 'lorem ipsum',
+    image: 'https://i.ibb.co/gSK6gBR/image-43.png',
+    socials: {
+      github: 'https://tinyurl.com/5x2bcwjy',
+      discord: 'https://tinyurl.com/5x2bcwjy',
+      mail: 'https://tinyurl.com/5x2bcwjy',
+      website: 'https://tinyurl.com/5x2bcwjy'
+    }
+  },
+  {
+    name: 'Ariyan Behrouz',
+    position: 'President',
+    quote: 'lorem ipsum',
+    image: 'https://i.ibb.co/gSK6gBR/image-43.png',
+    socials: {
+      // github: 'https://tinyurl.com/5x2bcwjy',
+      discord: 'https://tinyurl.com/5x2bcwjy',
+      mail: 'https://tinyurl.com/5x2bcwjy',
+      website: 'https://tinyurl.com/5x2bcwjy'
+    }
+  },
+  {
+    name: 'Ariyan Behrouz',
+    position: 'President',
+    quote: 'lorem ipsum',
+    image: 'https://i.ibb.co/gSK6gBR/image-43.png',
+    socials: {
+      github: 'https://tinyurl.com/5x2bcwjy',
+      discord: 'https://tinyurl.com/5x2bcwjy',
+      mail: 'https://tinyurl.com/5x2bcwjy',
+      website: 'https://tinyurl.com/5x2bcwjy'
+    }
+  },
+  {
+    name: 'Ariyan Behrouz',
+    position: 'President',
+    quote: 'lorem ipsum',
+    image: 'https://i.ibb.co/gSK6gBR/image-43.png',
+    socials: {
+      github: 'https://tinyurl.com/5x2bcwjy',
+      discord: 'https://tinyurl.com/5x2bcwjy',
+      mail: 'https://tinyurl.com/5x2bcwjy',
+      website: 'https://tinyurl.com/5x2bcwjy'
+    }
+  }
+]
+
+export const events = [
+  {
+    id: 'E12312312',
+    title: 'Deep Fake Technology',
+    description:
+      'Are you ready to dive into the world of Artificial Intelligence? Join us for an exclusive event focused on the captivating realm of Deep Fake Technology! Dive into the fascinating intersection of AI and creativity, we present the most awaited collaboration of the year HCC Cyber Club x HCC Computer Science Association (CSA) with the biggest clubs at Houston Community College!',
+    event_date: new Date('May 1, 2024 18:00:00'),
+    address: `Houston Community College
+Room C 145,
+West Loop Campus,
+West Loop South,
+Houston, TX`,
+    short_address: '5601 W Loop S, Houston',
+    time_end: '8:00pm',
+    registration_link: 'https://tinyurl.com/5x2bcwjy',
+    image: 'https://i.ibb.co/h9Bs2p8/NAFEB232024-1.png'
+  },
+  {
+    id: 'E12312223',
+    title: 'Deep Fake Technology',
+    description:
+      'Are you ready to dive into the world of Artificial Intelligence? Join us for an exclusive event focused on the captivating realm of Deep Fake Technology! Dive into the fascinating intersection of AI and creativity, we present the most awaited collaboration of the year HCC Cyber Club x HCC Computer Science Association (CSA) with the biggest clubs at Houston Community College!',
+    event_date: new Date('February 23, 2024 18:00:00'),
+    address: `Houston Community College
+Room C 145,
+West Loop Campus,
+West Loop South,
+Houston, TX`,
+    short_address: '5601 W Loop S, Houston',
+    time_end: '8:00pm',
+    registration_link: 'https://tinyurl.com/5x2bcwjy',
+    image: 'https://i.ibb.co/h9Bs2p8/NAFEB232024-1.png'
+  },
+  {
+    id: 'E123142312',
+    title: 'Deep Fake Technology',
+    description:
+      'Are you ready to dive into the world of Artificial Intelligence? Join us for an exclusive event focused on the captivating realm of Deep Fake Technology! Dive into the fascinating intersection of AI and creativity, we present the most awaited collaboration of the year HCC Cyber Club x HCC Computer Science Association (CSA) with the biggest clubs at Houston Community College!',
+    event_date: new Date('February 23, 2024 18:00:00'),
+    address: `Houston Community College
+Room C 145,
+West Loop Campus,
+West Loop South,
+Houston, TX`,
+    short_address: '5601 W Loop S, Houston',
+    time_end: '8:00pm',
+    registration_link: 'https://tinyurl.com/5x2bcwjy',
+    image: 'https://i.ibb.co/h9Bs2p8/NAFEB232024-1.png'
+  }
+]
+
+export const albums = [
+  {
+    id: 'A12345',
+    title: 'CSA Hackathon',
+    description:
+      'Lorem ipsum dolor sit amet. Est deserunt consequatur id soluta aliquam ut fuga recusandae in dolores eaque qui eligendi architecto et incidunt officia.',
+    date: new Date('October 20, 2023'),
+    photos: [
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png'
+    ]
+  },
+  {
+    id: 'A23456',
+    title: 'CSA Hackathon',
+    description:
+      'Lorem ipsum dolor sit amet. Est deserunt consequatur id soluta aliquam ut fuga recusandae in dolores eaque qui eligendi architecto et incidunt officia.',
+    date: new Date('October 20, 2023'),
+    photos: [
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png'
+    ]
+  },
+  {
+    id: 'A34567',
+    title: 'CSA Hackathon',
+    description:
+      'Lorem ipsum dolor sit amet. Est deserunt consequatur id soluta aliquam ut fuga recusandae in dolores eaque qui eligendi architecto et incidunt officia.',
+    date: new Date('October 20, 2023'),
+    photos: [
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png'
+    ]
+  },
+  {
+    id: 'A45678',
+    title: 'CSA Hackathon',
+    description:
+      'Lorem ipsum dolor sit amet. Est deserunt consequatur id soluta aliquam ut fuga recusandae in dolores eaque qui eligendi architecto et incidunt officia.',
+    date: new Date('October 20, 2023'),
+    photos: [
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://i.ibb.co/F3fWMBM/image-2024-03-13-182426986.png',
+      'https://www.giantfreakinrobot.com/wp-content/uploads/2022/08/rick-astley.jpg'
+    ]
+  }
+]
+
+export const resources = [
+  {
+    slug: 'computer-science',
+    type: 'Computer Science',
+    description: `Level up your CS fundamentals! Explore this section brimming with essential resources on core computer science concepts, programming languages, algorithms, and data structures. Whether you're a seasoned coder or just starting out, you'll find valuable materials to solidify your understanding and sharpen your programming skills.`,
+    image: 'https://i.ibb.co/mtwBH7c/what-is-computer-science-banner1600.jpg',
+    resource_collection: [
+      {
+        id: 'R000000001',
+        title: 'Getting Started',
+        resource_materials: [
+          { title: 'Base CS', link: 'https://medium.com/basecs' },
+          { title: 'Tutorials Point', link: 'https://www.tutorialspoint.com/' }
+        ]
+      },
+      {
+        id: 'R000000002',
+        title: 'Computer Architechture',
+        resource_materials: [
+          {
+            title: 'Computer Architechture',
+            link: 'https://github.com/the-akira/computer_science_web_resources/blob/master/db/computer_architecture.md'
+          },
+          {
+            title: 'Operating Systems',
+            link: 'https://github.com/the-akira/Computer-Science-Resources/blob/master/db/operating_systems.md'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    slug: 'platforms',
+    type: 'Platforms',
+    description: `Take your coding skills to the next level! This section offers a curated selection of resources specifically designed to help you excel in competitive programming, hone your hacking abilities, and conquer the vast landscape of MOOCs (Massive Open Online Courses).  Dive in, challenge yourself, and unlock your full coding potential!`,
+    image: 'https://images.pexels.com/photos/276452/pexels-photo-276452.jpeg',
+    resource_collection: [
+      {
+        id: 'R000000003',
+        title: 'Competitive Programming',
+        resource_materials: [
+          { title: 'Hackerrank', link: 'https://www.hackerrank.com' },
+          { title: 'CodeChef', link: 'https://www.codechef.com' }
+        ]
+      },
+      {
+        id: 'R000000004',
+        title: 'Information & Security',
+        resource_materials: [
+          {
+            title: 'Beginner Bug Bounty Hunters resources',
+            link: 'https://github.com/nahamsec/Resources-for-Beginner-Bug-Bounty-Hunters'
+          },
+          {
+            title: 'Web Hacking',
+            link: 'https://github.com/the-akira/Computer-Science-Resources/blob/master/db/web_hacking.md'
+          }
+        ]
+      }
+    ]
+  }
+]
